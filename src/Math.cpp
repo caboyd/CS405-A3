fdf9420506825778e174eb68aab7283c54b8e75d
Chris <boyd111c@gmail.com>
2018-09-26 11:34:21 -0600

    Added comments.

--- src/Math.cpp
+++ src/Math.cpp
@@ -2,18 +2,28 @@
 #include "Mat4.h"
 #include <iostream>
 
+
 void orthoNormalVectors(const Vector3& v1, const Vector3& v2, Vector3& u, Vector3& v, Vector3& n)
 {
+	//v2 is the preferred up vector, but not necessarily the exact up vector
 	const Vector3& preferred_up = v2;
 
+	//The normal is the direction of v1 and will be along
+	//the Z axis in our new coordinate system.
 	const Vector3 z_forward = v1.getNormalized();
+	//n is called the forward vector because it is the direction the normal is facing
 	n = z_forward;
 
+
+	//Y cross Z gives +X because we are in right handed coordinate system
 	Vector3 x_right = preferred_up.cross(z_forward);
 	x_right.normalize();
+	//u is called the right vector because positive X is to the right
 	u = x_right;
 
+	//Z cross X gives +Y because we are in right handed coordinate system
 	const Vector3 y_up = z_forward.cross(x_right);
+	//v is called the up vector because positive Y is up.
 	v = y_up;
 }
 
@@ -21,7 +31,7 @@ void coordinateSystemTransformationMatricesFromPositionNormalUp(const Vector3& V
                                                Mat4& transformation_matrix,
                                                Mat4& inverse_transformation_matrix)
 {
-		Vector3 u, v, n;
+	Vector3 u, v, n;
 	orthoNormalVectors(VPN, VUP, u, v, n);
 
 	Mat4 R = Mat4(u.x, u.y, u.z, 0.f,
--- src/main.cpp
+++ src/main.cpp
@@ -5,41 +5,50 @@
 #include <vector>
 
 #include "Math.h"
-
-
 using std::cout;
+using std::endl;
 
-void print_test(const Vector3& VRP, const Vector3& VPN, const Vector3& VUP, std::string message, std::vector<Vector3> test_points);
+/**
+ *	Creates transformation and inverse transformation matrices and prints them
+ *	Uses the given test points and uses the transformation matrix to transform them to the new coordinate system.
+ *	Prints the test input point and output point after is has been transformed.
+ *
+ */
+void print_test(const Vector3& VRP, const Vector3& VPN, const Vector3& VUP, std::string message,
+                std::vector<Vector3> test_points);
 
 int main()
 {
-	Vector3 VRP(5.f,5.f,5.f), VPN(0.f,0.f,-1.f), VUP(0.f,1.f,0.1f); 
+	Vector3 VRP(5.f, 5.f, 5.f), VPN(0.f, 0.f, -1.f), VUP(0.f, 1.f, 0.1f);
 
 	cout << "---- TEST #1 ----\n";
-	print_test(Vector3(0.f,0.f,0.f),Vector3(0.f,0.f,1.f),Vector3(0.f,1.f,0.f), 
-		"The generated matrix should be an identity matrix",
-		{});
-	cout << "---- TEST #2 ----\n";
-	print_test(Vector3(0.f,0.f,0.f),Vector3(1.f,0.f,0.f),Vector3(0.f,1.f,0.f),
-		"The generated matrix should be a rotation matrix around y-axis for -90 degrees.",
-		{{1.f,0.5f,0.f},{0.f,1.f,-2.f}});
-		cout << "---- TEST #3 ----\n";
-	print_test(Vector3(0.f,0.f,0.f),Vector3(0.f,1.f,1.f),Vector3(0.f,1.f,0.f),
-		"The generated matrix should be a rotation matrix around x-axis for 45 degrees.",
-		{{0.f,1.f,0.f},{1.f,0.707f,0.707f}});
-		cout << "---- TEST #4 ----\n";
-	print_test(Vector3(1.f,1.f,1.f),Vector3(0.f,1.f,1.f),Vector3(0.f,1.f,0.f),
-		"The generated matrix should be a combination of translation of (-1.0, -1.0, -1.0) and rotation matrix around x-axis for 45 degrees.",
-		{{0.f,1.f,0.f},{1.f,0.707f,0.707f}});
+	print_test(Vector3(0.f, 0.f, 0.f), Vector3(0.f, 0.f, 1.f), Vector3(0.f, 1.f, 0.f),
+	           "The generated matrix should be an identity matrix",
+	           {});
 
+	cout << "---- TEST #2 ----\n";
+	print_test(Vector3(0.f, 0.f, 0.f), Vector3(1.f, 0.f, 0.f), Vector3(0.f, 1.f, 0.f),
+	           "The generated matrix should be a rotation matrix around y-axis for -90 degrees.",
+	           {{1.f, 0.5f, 0.f}, {0.f, 1.f, -2.f}});
+
+	cout << "---- TEST #3 ----\n";
+	print_test(Vector3(0.f, 0.f, 0.f), Vector3(0.f, 1.f, 1.f), Vector3(0.f, 1.f, 0.f),
+	           "The generated matrix should be a rotation matrix around x-axis for 45 degrees.",
+	           {{0.f, 1.f, 0.f}, {1.f, 0.707f, 0.707f}});
+
+	cout << "---- TEST #4 ----\n";
+	print_test(Vector3(1.f, 1.f, 1.f), Vector3(0.f, 1.f, 1.f), Vector3(0.f, 1.f, 0.f),
+	           "The generated matrix should be a combination of translation of (-1.0, -1.0, -1.0) and rotation matrix around x-axis for 45 degrees.",
+	           {{0.f, 1.f, 0.f}, {1.f, 0.707f, 0.707f}});
 }
 
-void print_test(const Vector3& VRP, const Vector3& VPN, const Vector3& VUP, std::string message, std::vector<Vector3> test_points)
+void print_test(const Vector3& VRP, const Vector3& VPN, const Vector3& VUP, std::string message,
+                std::vector<Vector3> test_points)
 {
-	cout << message << std::endl;
-	cout << "VRP = " << VRP << std::endl;
-	cout << "VPN = " << VPN << std::endl;
-	cout << "VUP = " << VUP << std::endl << std::endl;
+	cout << message << endl;
+	cout << "VRP = " << VRP << endl;
+	cout << "VPN = " << VPN << endl;
+	cout << "VUP = " << VUP << endl << endl;
 
 	Vector3 n, u, v;
 	orthoNormalVectors(VPN, VUP, u, v, n);
@@ -49,20 +58,19 @@ void print_test(const Vector3& VRP, const Vector3& VPN, const Vector3& VUP, std:
 	// cout << "u_x_right: " << u << std::endl;
 
 	Mat4 matrix, inverse_matrix;
-	coordinateSystemTransformationMatricesFromPositionNormalUp(VRP,VPN,VUP,matrix,inverse_matrix);
+	coordinateSystemTransformationMatricesFromPositionNormalUp(VRP, VPN, VUP, matrix, inverse_matrix);
 
 	cout << "The generated transformation matrix is:\n";
-	cout << matrix << std::endl;
-	
-	cout << "The generated inverse transformation matrix is:\n";
-	cout << inverse_matrix << std::endl;
+	cout << matrix << endl;
 
-	if(test_points.empty()) return;
+	cout << "The generated inverse transformation matrix is:\n";
+	cout << inverse_matrix << endl;
+
+	if (test_points.empty()) return;
 
 	cout << "Testing Points:\n";
-	for(Vector3& point : test_points)
-	{
-		cout << point << " -> " << matrix * point << std::endl;
-	}
-	cout << std::endl;
+	for (Vector3& point : test_points)
+		cout << point << " -> " << matrix * point << endl;
+
+	cout << endl;
 }
